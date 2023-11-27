f1d2cd50dc6a01aa854005f943617b0d3be410bc
Jovenayo <80540224+Jovenayo@users.noreply.github.com>
2023-11-27 18:38:38 +0100

    Update Servidor.c

--- Servidor.c
+++ Servidor.c
@@ -5,12 +5,11 @@
 #include <sys/wait.h>
 #include <pthread.h>
 
-#define DIRECCION
-#define PORT 8080
+#define PORT 8080;
 #define MAX_BUFFER_SIZE 1024;
-const char *ficehroGuardado = "fichero.txt"; //Dieccion del fichero de guardado.
+#define FICEHRO_GUARDADO "fichero.txt"; //Dieccion del fichero de guardado.
 int fd; //Descriptor del fichero.
-struct sockaddr_in serverEstaciones_address, clienteEstacion_address, serverClientes_address, clienteCliente_address; 
+struct sockaddr_in serverEstaciones_address, clienteEstacion_address, serverClientes_address, clienteCliente_address; //Estructuras para almacenar las direcciones de conexion.
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 socklen_t addr_size;
 
@@ -43,7 +42,7 @@ void GestorClientes(){
 	//Configurar direccion del servidor
 	serverClientes_address.sin_family = AF_INET;
 	serverClientes_address.sin_port = htons(PORT);
-	serverClientes_address.sin_addr.s_addr = INADDR_ANY;
+	serverClientes_address.sin_addr.s_addr = INADDR_ANY;//Direccion del server (INADDR-ANY: Conexiones a cualquier interfaz del sistea.)
 	
 	//Enlazar soket a la direcicon y puerto.
 	if(bind(serverCliente_socket, (struct soketaddr*)&serverClientes_address, sizeof(serverClientes_address)) < 0){
@@ -85,14 +84,30 @@ void GestorClientes(){
 
 void Cliente(int clienteCliente_socket){
 	char buffer[1024];
-
-	//Recivir la accion a realizar
+	char opc[1024];
+	//Recivir la opcion a realizar
 	size_t bytes_recividos = recv(clienteCliente_socket, buffer, sizeof(buffer), 0);
-	
+	recv(clienteCliente_socket, opc, sizeof(opt), 0)
+	int opcion = atoi(opc)
+
+
 	if(bytes_recividos > 0){
 		//Crear el case, con las respectivas funciones
-			//1: Obtener datos del fichero
-			//2: Obtener datos de una estacion
+		switch (opcion) {
+			case 1://: Obtener datos del fichero
+
+				break;
+			case 2://: Obtener datos de una estacion
+				//Obtenemos Id Estacion del soket
+				getDatos(clienteCliente_socket, clie);
+				break;
+			case 3:
+			
+				break;
+			default:
+			printf("Opción no valida.\n");
+  		}
+			
 		
     	} else { 
         printf("Error al recibir datos del socket");
@@ -116,7 +131,7 @@ void GestorEstaciones(){
 	//Configurar direccion del servidor
 	serverEstaciones_address.sin_family = AF_INET;
 	serverEstaciones_address.sin_port = htons(PORT);
-	serverEstaciones_address.sin_addr.s_addr = INADDR_ANY;
+	serverEstaciones_address.sin_addr.s_addr = INADDR_ANY;//Direccion del server (INADDR-ANY: Conexiones a cualquier interfaz del sistea.)
 	
 	//Enlazar soket a la direcicon y puerto.
 	if(bind(serverEstacion_socket, (struct soketaddr*)&serverEstaciones_address, sizeof(serverEstaciones_address)) < 0){
@@ -182,4 +197,41 @@ void Estacion(int clienteEstacion_socket){ //Zona de riesgo para escribir el fic
         printf("Error al recibir datos del socket");
         }
     close(clienteEstacion_socket);
+}
+
+
+//Funciones de gestion del fichero
+void getDato(int clienteCliente_soket){
+	// Abrimos el archivo en modo lectura
+	FILE *f = fopen(FICEHRO_GUARDADO, "r");
+	char idEstacion[1024];
+	
+	recv(clienteCliente_socket, idEstacion, sizeof(idEstacion), 0)
+	
+	// Si el archivo no existe, imprimimos un mensaje de error
+	if (f == NULL) {
+		printf("El archivo no existe.\n");
+	return 1;
+	}
+	
+	// Leemos cada línea del archivo
+	while (!feof(f)) {
+		// Obtenemos la línea del archivo
+		char linea[1024];
+		fgets(linea, sizeof(linea), f);
+	
+		// Obtenemos el primer campo de la línea
+		char *campo_1 = strtok(linea, ":");
+	
+		// Si el primer campo es igual al número, enviamos la línea
+		if (atoi(campo_1) == atoi(idEstacion)) {
+			send(clienteCliente_socket, linea, sizeof(linea), 0);
+		}
+	}
+	close(clienteCliente_socket)
+	// Cerramos el archivo
+	fclose(f);
+	
+	return 0;
+	}
 }
